b25997cef080607c9e6af45e67d8550b28d3ac99
Jung (JK) Kim <jung.kim.3627@gmail.com>
2021-08-01 09:19:44 -0700

    tests

--- tests/shared/spec.filter.js
+++ tests/shared/spec.filter.js
@@ -5,6 +5,7 @@ const filter = require('../../js/shared/filter');
 const moment = require('../../js/helpers/moment');
 const testUtils = require('../testUtils');
 const env = require('../../js/env');
+const sinon = require('sinon');
 
 
 describe('filter.js', () => {
@@ -12,16 +13,58 @@ describe('filter.js', () => {
         testUtils.reset();
     });
 
-    it('setStart', () => {
+    describe('setStart', () => {
+        it('null', () => {
+            filter.setStart(null);
 
+            sinon.assert.notCalled(eventSignals.dispatch.withArgs());
+        });
+
+        it('default', () => {
+            const time = moment('2020-01-01 10:00');
+
+            filter.setStart(time);
+
+            sinon.assert.calledOnce(eventSignals.dispatch.withArgs({
+                event: 'filter.change',
+                changed: { start: time },
+            }));
+            assert.deepEqual(env._filter._start, time);
+        });
+    });
+
+    it('getStart', () => {
         const time = moment('2020-01-01 10:00');
-
         filter.setStart(time);
 
-        eventSignals.dispatch.calledWith({
-            event: 'filter.change',
-            changed: { start: time },
+        assert.equal(filter.getStart(), time);
+    });
+
+
+    describe('setEnd', () => {
+        it('null', () => {
+            filter.setEnd(null);
+
+            sinon.assert.notCalled(eventSignals.dispatch.withArgs());
         });
-        assert.deepEqual(env._filter._start, time);
+
+        it('default', () => {
+            const time = moment('2020-01-01 10:00');
+
+            filter.setEnd(time);
+
+            sinon.assert.calledOnce(eventSignals.dispatch.withArgs({
+                event: 'filter.change',
+                changed: { end: time },
+            }));
+            assert.deepEqual(env._filter._end, time);
+        });
+    });
+
+    it('getEnd', () => {
+        const time = moment('2020-01-01 10:00');
+        filter.setEnd(time);
+
+        assert.equal(filter.getEnd(), time);
     });
 });
